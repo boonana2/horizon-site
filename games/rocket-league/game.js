07cbcf32b1d32464249273566b6c99ea6f803902
boonana <WilsonW262@outlook.com>
2026-07-19 15:45:21 -0700

    Update game.js

--- games/rocket-league/game.js
+++ games/rocket-league/game.js
@@ -21,7 +21,7 @@ document.getElementById("start-play").addEventListener("click", () => {
     // Create the iframe dynamically
     const iframe = document.createElement("iframe");
     iframe.id = "game-iframe";
-    iframe.src = "rocketleague.html"; // Load the game cleanly
+    iframe.src = "rocketsoccerderby.html"; // Load the game cleanly
     
     // Append it to our container
     container.appendChild(iframe);
